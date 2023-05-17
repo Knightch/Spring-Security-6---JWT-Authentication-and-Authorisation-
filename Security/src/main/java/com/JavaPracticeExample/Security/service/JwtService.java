ee087c9ec4c370d05c4387b617873116d9390702
knightch <amankumarpandey2397@gmail.com>
2023-05-18 00:09:21 +0530

    create token with cliams and without cliams

--- Security/src/main/java/com/JavaPracticeExample/Security/service/JwtService.java
+++ Security/src/main/java/com/JavaPracticeExample/Security/service/JwtService.java
@@ -2,11 +2,17 @@ package com.JavaPracticeExample.Security.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Service
@@ -15,7 +21,7 @@ public class JwtService {
     private static final String SECRET_KEY = "50655368566D597133743677397A24432646294A404D635166546A576E5A7234";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
@@ -30,6 +36,21 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return  generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Key getSignInKey() {

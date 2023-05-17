e7e039ec19f6a5f94827c513af75f889593414e0
knightch <amankumarpandey2397@gmail.com>
2023-05-17 22:58:20 +0530

    extract user details from jwt token!

--- Security/src/main/java/com/JavaPracticeExample/Security/config/JwtAuthenticationFilter.java
+++ Security/src/main/java/com/JavaPracticeExample/Security/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.JavaPracticeExample.Security.config;
 
+import com.JavaPracticeExample.Security.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,14 +14,19 @@ import java.io.IOException;
 @Component
 @AllArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }
---
+++ Security/src/main/java/com/JavaPracticeExample/Security/service/JwtService.java
@@ -0,0 +1,11 @@
+package com.JavaPracticeExample.Security.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return email;
+    }
+}

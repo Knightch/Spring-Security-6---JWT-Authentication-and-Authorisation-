175ac8438fda8eb794bb6b1be9eb9b4a8828b4b6
knightch <amankumarpandey2397@gmail.com>
2023-05-17 23:24:21 +0530

    extracr jwt claims!

--- Security/src/main/java/com/JavaPracticeExample/Security/service/JwtService.java
+++ Security/src/main/java/com/JavaPracticeExample/Security/service/JwtService.java
@@ -1,11 +1,22 @@
 package com.JavaPracticeExample.Security.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
     public String extractUsername(String token) {
-        return email;
+        return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSingInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }

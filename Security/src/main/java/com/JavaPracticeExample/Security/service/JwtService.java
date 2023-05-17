92f81832db3c660c66535c0c6c8ab93cacdf756d
knightch <amankumarpandey2397@gmail.com>
2023-05-17 23:39:05 +0530

    extract all type of cliam!

--- Security/src/main/java/com/JavaPracticeExample/Security/service/JwtService.java
+++ Security/src/main/java/com/JavaPracticeExample/Security/service/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -17,16 +18,21 @@ public class JwtService {
         return null;
     }
 
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
     private Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
-                .setSigningKey(getSingInKey())
+                .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
 
-    private Key getSingInKey() {
+    private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }

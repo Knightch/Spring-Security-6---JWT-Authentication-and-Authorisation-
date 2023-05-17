4e7b2b4870dce3b3cced3d509144d7f1dea58045
knightch <amankumarpandey2397@gmail.com>
2023-05-17 23:34:17 +0530

    implements signInKey

--- Security/src/main/java/com/JavaPracticeExample/Security/service/JwtService.java
+++ Security/src/main/java/com/JavaPracticeExample/Security/service/JwtService.java
@@ -2,10 +2,16 @@ package com.JavaPracticeExample.Security.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "50655368566D597133743677397A24432646294A404D635166546A576E5A7234";
 
     public String extractUsername(String token) {
         return null;
@@ -18,5 +24,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSingInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }

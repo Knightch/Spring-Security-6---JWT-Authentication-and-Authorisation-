6a40c734d6c1a4b5335dabb3e283ac05836b7aa8
knightch <amankumarpandey2397@gmail.com>
2023-05-19 01:06:08 +0530

    all done!

---
+++ Security/src/main/java/com/JavaPracticeExample/Security/demo/DemoController.java
@@ -0,0 +1,13 @@
+package com.JavaPracticeExample.Security.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+public class DemoController {
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello from Secured endpoint!")
+    }
+}
